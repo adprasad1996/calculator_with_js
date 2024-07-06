a357eb6f83da7f1ac96c6a9d3f61fc9f8ecb3569
abdulshaik393 <aksk02340@gmail.com>
2024-07-06 18:57:34 +0530

    added title

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function appendToDisplay(value) {
       const result = eval(document.getElementById('display').value);
       document.getElementById('display').value = result;
     } catch (error) {
-      document.getElementById('display').value = 'Error';
+      document.getElementById('display').value = 'Error';4
     }
   }
   
